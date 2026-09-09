f7c570274304e1ac1556c866b4015c40548c0531
riveroalejo16-cloud <riveroalejo16@gmail.com>
2026-09-08 23:11:27 -0500

    Corregir collage para conservar proporción completa de las fotos

--- public/background.js
+++ public/background.js
@@ -1,7 +1,5 @@
 (()=>{
-  const ids=[
-    '10-nuLaaNRKUPQOQvJwP5fMfNL2tPQsUy','1AEnQBHJhIC62itL9Il431pB9ZMEg9T-a','1FsYgnQpbnYqECGYcKAXOqMCgdwybSmuM','1JIdUk-e-WBqIyiD74Dxq-bKSsTOLLA2-','1LQJB9aYtn35hM17iG1OS_zNClfb-13t-','1MswpaehCQP29qjM8r44a3EBZqU5vgLKc','1OwuOISBa72Y7fR-o4ms24nm5j3gbR8MA','1U7s5W5tM98GwTTBdkwTISB96PBQakI1d','1VEgH7jzY4e9j9jJPJhfr-ML-0FhHc_KK','1Yxcpi4xs3YDgMiM-eP90-r09FLlLJXH1','1hsktHkL1PBdZppzLx4ZOu91zrd4NhSmG','1kYblIlez3QOWhJhPmptC7i120_gzWH4Y','1pNYsaoRnYKu2HwqNmoQ-0RkfOr5WFicV'
-  ];
+  const ids=['10-nuLaaNRKUPQOQvJwP5fMfNL2tPQsUy','1AEnQBHJhIC62itL9Il431pB9ZMEg9T-a','1FsYgnQpbnYqECGYcKAXOqMCgdwybSmuM','1JIdUk-e-WBqIyiD74Dxq-bKSsTOLLA2-','1LQJB9aYtn35hM17iG1OS_zNClfb-13t-','1MswpaehCQP29qjM8r44a3EBZqU5vgLKc','1OwuOISBa72Y7fR-o4ms24nm5j3gbR8MA','1U7s5W5tM98GwTTBdkwTISB96PBQakI1d','1VEgH7jzY4e9j9jJPJhfr-ML-0FhHc_KK','1Yxcpi4xs3YDgMiM-eP90-r09FLlLJXH1','1hsktHkL1PBdZppzLx4ZOu91zrd4NhSmG','1kYblIlez3QOWhJhPmptC7i120_gzWH4Y','1pNYsaoRnYKu2HwqNmoQ-0RkfOr5WFicV'];
   const urls=ids.map(id=>`https://drive.google.com/thumbnail?id=${id}&sz=w1600`);
   const collage=document.createElement('div');
   collage.className='photo-collage';
@@ -11,13 +9,14 @@
   const tiles=[...collage.querySelectorAll('.photo-tile')];
   const preload=urls.map(src=>new Promise(resolve=>{const im=new Image();im.onload=()=>resolve(src);im.onerror=()=>resolve(src);im.src=src;}));
   Promise.all(preload).then(()=>{
-    tiles.forEach((tile,i)=>{tile.querySelector('img').src=urls[i];tile.classList.add('visible')});
+    const show=(tile,src)=>{tile.style.setProperty('--photo',`url("${src}")`);tile.querySelector('img').src=src;tile.classList.add('visible')};
+    tiles.forEach((tile,i)=>show(tile,urls[i]));
     let step=4;
     setInterval(()=>{
       const tile=tiles[(step-4)%4];
-      const img=tile.querySelector('img');
+      const src=urls[step%urls.length];
       tile.classList.remove('visible');
-      setTimeout(()=>{img.src=urls[step%urls.length];tile.classList.add('visible')},420);
+      setTimeout(()=>show(tile,src),420);
       step++;
     },1800);
   });
